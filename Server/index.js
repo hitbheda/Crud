2a5a547639faa4cf7d8dff5b3446f5fb083b37bb
hitbheda <zayoxop666@gmail.com>
2024-05-17 17:05:00 +0530

    Hii

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = 3000;
 
+// Database Connection
 mongoose.connect('mongodb+srv://User-6355:User-6355@cluster0.ljlfrcm.mongodb.net/User',{
     useNewUrlParser:true,
     useUnifiedTopology:true
@@ -14,8 +15,29 @@ mongoose.connect('mongodb+srv://User-6355:User-6355@cluster0.ljlfrcm.mongodb.net
     process.exit(1)
 })
 
+// Create Schema
+const userSchema = new mongoose.Schema({
+    name:String,
+    email: String,
+    age:Number
+})
 
+// Create Model Based On Schema 
+const User = mongoose.model('User',userSchema);
 
+// Middleware
+app.use(bodyParser.json())
+
+//Routes
+app.post('/user',async(req,res)=>{
+    try{
+        const user = new User(req.body);
+        await user.save();
+        res.status(200).send(user);
+    }catch(err){
+        res.status(400).send(err)
+    }
+})
 app.get('/',(req,res)=>{
     res.send("This Is Root Path")
 })
